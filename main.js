5709372318899ab34e0026c10e6420b590109400
Ahaan-Mandal <122046904+Ahaan-Mandal@users.noreply.github.com>
2023-01-13 17:58:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload(){
+
+}
+
+function setup() {
+    canvas=createCanvas(650,500);
+    canvas.position(110,250);
+    video= createCapture(VIDEO);
+    video.hide();
+
+    tint_colour=""
+}
+
+function draw(){
+    image(video,0,0,650,500);
+    tint(tint_colour);
+}
+
+function take_snapshot(){
+    save('Ahaan_Photo.png');
+}
+
+function filter_tint(){
+    tint_colour=document.getElementById("color_name").value;
+}
